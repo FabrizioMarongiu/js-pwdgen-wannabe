b738a947408ab33cbd6efefa2a5192ee0d0fdff6
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-03-18 17:20:19 +0100

    End

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ document.getElementById('first-message').innerHTML='Ciao!In questo sito potrai c
 
 //NAME MESSAGE
 var name = prompt('Inserisci il tuo nome');
-console.log(name);
+console.log('name ' + name);
 
 document.getElementById('realname').innerHTML='Il tuo nome è ' + name;
 
 //SURNAME MESSAGE
 var surname = prompt('Inserisci il tuo cognome');
-console.log(surname);
+console.log('surname ' + surname);
 
 document.getElementById('realsurname').innerHTML = 'Il tuo cognome è ' + surname;
 
 //COLOR MESSAGE
 var color = prompt('Inserisci il tuo colore preferito');
-console.log(color);
+console.log('color ' + color);
 
 document.getElementById('realcolor').innerHTML='Il tuo colore preferito è ' + color;
+
+//ADD 21
+var secretNumber = 21;
+//RETURN PASSWORD
+console.log('secure password is ' + name + surname + color + secretNumber);
+
+document.getElementById('password').innerHTML = 'La tua password sicurissima è ' + name + surname + color + secretNumber;
+
+//LAST MESSAGE
+alert('Ti svelo un segreto...Vuoi sapere qualè?');
+
+document.getElementById('last-message').innerHTML='Questa è la password meno sicura che portesse capitarti...Non utilizzarla mai!!';
